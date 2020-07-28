b1528aaa5cea9410318a9a4f270e932a6a70c59b
tinytelly <tinytelly@users.noreply.github.com>
2020-07-28 14:07:14 +1000

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -2,7 +2,7 @@ multibranchPipelineJob('configuration-as-code') {
           branchSources {
               git {
                   id = 'configuration-as-code'
-                  remote('https://github.com/tinytelly/flux-get-started2.git')
+                  remote('https://github.com/tinytelly/flux-get-started.git')
               }
           }
       }
